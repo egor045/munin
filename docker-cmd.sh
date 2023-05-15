26b553aed4ff0fd5effead2e324c16e7f47879eb
Kroese <kroese@users.noreply.github.com>
2023-05-15 13:47:01 +0200

    Remove support for local node

--- docker-cmd.sh
+++ docker-cmd.sh
@@ -29,15 +29,6 @@ sudo -u munin -- /usr/sbin/rrdcached \
   -m 0660 -l unix:/run/munin/rrdcached.sock \
   -w 1800 -z 1800 -f 3600
 
-if [ "$DISABLE_MUNIN_NODE" != "true" ]; then
-
-  # Configure munin node in the docker
-  munin-node-configure --shell --suggest 2>/dev/null | bash
-  
-  NODES="Munin;localhost:127.0.0.1 $NODES"
-  
-fi
-
 # Generate node list
 [[ ! -z "$NODES" ]] && for NODE in $NODES
 do
@@ -77,18 +68,7 @@ env.community $COMMUNITY
 
 EOF
   fi
-  [ "$DISABLE_MUNIN_NODE" != "true" ] && munin-node-configure --shell --snmp "$HOST" --snmpcommunity "$COMMUNITY" | bash
 done
-
-if [ "$DISABLE_MUNIN_NODE" != "true" ]; then
-
-  # Remove plugins that doesn't work in docker
-  rm /etc/munin/plugins/{cpuspeed,open_files,users,swap,proc_pri}
-
-  # Start munin node on the docker
-  munin-node
-  
-fi
 
 # Run once before we start fcgi
 sudo -u munin -- /usr/bin/munin-cron munin
